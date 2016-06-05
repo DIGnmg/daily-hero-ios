85b8736a3828ce737af541c0a154f3250ba8ce1e
Adam Reeves <dark.account@gmail.com>
2016-06-04 23:18:32 -0500

    Add Character Image; Add linear gradient to background; Select color from image in background gradient;

---
+++ Marvel Explorer/RadialGradientLayer.swift
@@ -0,0 +1,65 @@
+//
+//  RadialGradientLayer.swift
+//  Marvel Explorer
+//
+//  Created by Adam Reeves on 6/4/16.
+//  Copyright © 2016 dignmg. All rights reserved.
+//
+
+import UIKit
+
+class RadialGradientLayer: CALayer {
+	
+	override init(){
+		
+		super.init()
+		
+		needsDisplayOnBoundsChange = true
+	}
+	
+	init(center:CGPoint,radius:CGFloat,colors:[CGColor]){
+		
+		self.center = center
+		self.radius = radius
+		self.colors = colors
+		
+		super.init()
+		
+	}
+	
+	required init(coder aDecoder: NSCoder) {
+		
+		super.init()
+
+	}
+	
+	var center:CGPoint = CGPointMake(50.0,50.0)
+	var radius:CGFloat = 20.0
+	var colors:[CGColor] = [
+		UIColor(red: 251/255, green: 237/255, blue:  33/255, alpha: 1.0).CGColor,
+		UIColor(red: 251/255, green: 179/255, blue: 108/255, alpha: 1.0).CGColor
+	]
+	
+	override func drawInContext(ctx: CGContext) {
+		
+		CGContextSaveGState(ctx)
+		
+		let colorSpace = CGColorSpaceCreateDeviceRGB()
+		let locations:[CGFloat] = [0.0, 1.0]
+		let gradient = CGGradientCreateWithColors(colorSpace, colors, locations)
+		
+		//var startPoint = CGPointMake(0, self.bounds.height)
+		//var endPoint = CGPointMake(self.bounds.width, self.bounds.height)
+		
+		CGContextDrawRadialGradient(
+			ctx, gradient,
+			center, 0.0,
+			center, radius,
+			CGGradientDrawingOptions.DrawsAfterEndLocation
+		)
+		
+		CGContextRestoreGState(ctx)
+		
+	}
+	
+}
--- Marvel Explorer/ViewController.swift
+++ Marvel Explorer/ViewController.swift
@@ -7,20 +7,27 @@
 //
 
 import UIKit
+import UIImageColors
 
 class ViewController: UIViewController {
     
     @IBOutlet var charNameLabel: UILabel!
+	@IBOutlet var charDescLabel: UILabel!
+	@IBOutlet var charImage: CharacterUIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+		// Do any additional setup after loading the view, typically from a nib.
     }
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
+		
+		
+		
         
-        MarvelService.sharedInstance.get("characters").query("name", value: "Spider-Man").call() {
+        MarvelService.sharedInstance.get("characters").query("name", value: "Hulk").call() {
             (char: [Character]) in
             self.printName(char[0])
         }
@@ -33,6 +40,7 @@ class ViewController: UIViewController {
     
     func printName(char: Character) -> Void {
         charNameLabel.text = char.name
+		charDescLabel.text = char.description
         downloadImage(char)
     }
     
@@ -42,8 +50,32 @@ class ViewController: UIViewController {
             dispatch_async(dispatch_get_main_queue()) { () -> Void in
                 guard let data = data where error == nil else { return }
                 print(response?.suggestedFilename ?? "")
-                print("Download Finished")
+
                 self.charImage.image = UIImage(data: data)
+				
+				self.charImage.image!.getColors({
+					(colors) in
+//					self.view.backgroundColor = colors.backgroundColor.CGColor
+
+					let colors = [
+						// colors.primaryColor, colors.secondaryColor, colors.detailColor
+						colors.secondaryColor.CGColor,
+						UIColor.init(red:   3/255, green: 53/255, blue: 70/255, alpha: 1.0).CGColor
+					]
+
+					let locations = [0.0, 1.0];
+					let gradient: CAGradientLayer = CAGradientLayer()
+					gradient.colors = colors
+					gradient.locations = locations
+					gradient.startPoint = CGPoint(x: 1.0, y: 0.0)
+					gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
+					gradient.frame = CGRect(x: 0.0, y: 0.0, width: self.view.frame.size.width, height: self.view.frame.size.height)
+
+					//let gradient = RadialGradientLayer(center: CGPointMake(50.0, 50.0), radius: 200, colors: colors)
+					//gradient.colors = colors
+					//gradient.setNeedsDisplay()
+					self.view.layer.insertSublayer(gradient, atIndex: 0)
+				})
             }
         }
     }
